a859d83c868b2b2edb49eb9e5a59e4255e223065
Soru2403 <rusich@inbox.lv>
2025-11-01 00:50:14 +0200

    Firabase connetion update 5

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+ window.firebaseConfig = {
+  apiKey: "AIzaSyBCZ8HMlsLfeVt9eTOqDxKs02LZ7GfzWcA",
+  authDomain: "calendar-a5aa1.firebaseapp.com",
+  databaseURL: "https://calendar-a5aa1-default-rtdb.firebaseio.com",
+  projectId: "calendar-a5aa1",
+  storageBucket: "calendar-a5aa1.firebasestorage.app",
+  messagingSenderId: "438675886673",
+  appId: "1:438675886673:web:38698cd985ee7dfc325e70"
+ };
